a54296044cc55a97b7613123e71433d0f2fb27ce
aryamanr26 <aryamanr@umich.edu>
2025-12-28 17:41:08 +0000

    Deploying to gh-pages from @ aryamanr26/aryamanr26.github.io@f31ea1f8ac3b0af6f374109e0e6ab804d3f4dc20 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,6 +518,13 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/aryamanrao26", "_blank");
+        },
+      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
